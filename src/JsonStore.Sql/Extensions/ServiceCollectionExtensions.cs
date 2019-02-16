10925648d88df097645a4996f19d158bf67cda04
arleypadua <arleydf@gmail.com>
2019-02-16 15:06:22 +0100

    added IServiceCollection extensions

---
+++ src/JsonStore.Sql/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+using JsonStore.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JsonStore.Sql.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSqlServerJsonStore(this IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<IStoreDocuments, SqlServerDocumentStore>(provider => 
+                new SqlServerDocumentStore(new SqlConnection(connectionString)));
+
+            return services;
+        }
+    }
+}
